f495922d5f850a60a84c95e6ecd4dafb77fbeac6
Krushna <hardekrushna88@gmail.com>
2026-05-02 20:55:02 +0530

    chore: add basic skeleton

---
+++ src/main/java/com/krushna/flashflow/FlashflowApplication.java
@@ -0,0 +1,13 @@
+package com.krushna.flashflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krushna/flashflow/FlashflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krushna.flashflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
