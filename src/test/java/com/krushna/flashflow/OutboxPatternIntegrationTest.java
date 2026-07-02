a3b8a376034c34a70c9478839b2a88331bdaa99e
Krushna <hardekrushna88@gmail.com>
2026-07-02 22:29:09 +0530

    chore: remove unused members

--- src/test/java/com/krushna/flashflow/OutboxPatternIntegrationTest.java
+++ src/test/java/com/krushna/flashflow/OutboxPatternIntegrationTest.java
@@ -29,7 +29,6 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
